a4d75c58907047616b7e16fab33e0d0ee21b1c8e
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-07-12 16:36:21 +0700

    Update like routes
    It contains GET likes/dislikes, did (interacted) and POST likes/dislikes

--- server/src/routes/forum/Like.js
+++ server/src/routes/forum/Like.js
@@ -1,198 +1,277 @@
 const CommentLike = require("../../models/forum/comment/CommentLike");
+const Comment = require("../../models/forum/comment/Comment");
+const Post = require("../../models/forum/post/Post");
 const PostLike = require("../../models/forum/post/PostLike");
 const router = require("express").Router();
 const { JwtMiddleware } = require("../../utils/JWT");
+const { log } = require("../../utils/Logger");
 
-const countLikesByCommentId = async (commentId) => {
-  const likes = await CommentLike.count({
-    where: {
-      commentId,
-      isDislike: false,
-    },
-  });
-  const dislikes = await CommentLike.count({
-    where: {
-      commentId,
-      isDislike: true,
-    },
-  });
-  return { likes, dislikes };
-};
-const countLikesByPostId = async (postId) => {
-  const likes = await PostLike.count({
-    where: {
-      postId,
-      isDislike: false,
-    },
-  });
-  const dislikes = await PostLike.count({
-    where: {
-      postId,
-      isDislike: true,
-    },
-  });
-  return { likes, dislikes };
-};
+// routes
+// /likes?type=comment&id=1&like=true -> comment likes
+// /likes?type=post&id=1&like=true -> post likes
+// /likes?type=comment&id=1&like=false -> comment dislikes
+// /likes?type=post&id=1&like=false -> post dislikes
 
-const likeComment = async (commentId, userId, isDislike) => {
-  const like = await CommentLike.findOne({
-    where: {
-      commentId,
-      userId,
-    },
-  });
-  if (like) {
-    like.isDislike = isDislike;
-    await like.save();
-  } else {
-    await CommentLike.create({
-      commentId,
-      userId,
-      isDislike,
-    });
-  }
-};
+// if post is not PUBLISHED and requested by USER -> 404
+// if requested by MOD | ADMIN -> 200
 
-const likePost = async (postId, userId, isDislike) => {
-  const like = await PostLike.findOne({
-    where: {
-      postId,
-      userId,
-    },
-  });
-  if (like) {
-    like.isDislike = isDislike;
-    await like.save();
-  } else {
-    await PostLike.create({
-      postId,
-      userId,
-      isDislike,
-    });
-  }
-};
+router.get("/", async (req, res) => {
+  const { userId, role } = req;
+  // type: comment | post
+  // id: commentId | postId
+  // like: true | false -> like | dislike
+  const { type, id, like } = req.query;
 
-const unlikeComment = async (commentId, userId) => {
-  const like = await CommentLike.findOne({
-    where: {
-      commentId,
-      userId,
-    },
-  });
-  if (like) {
-    await like.destroy();
-  }
-};
-
-const unlikePost = async (postId, userId) => {
-  const like = await PostLike.findOne({
-    where: {
-      postId,
-      userId,
-    },
-  });
-  if (like) {
-    await like.destroy();
-  }
-};
-
-const getLikes = async (req, res) => {
   try {
-    const { postId, commentId } = req.query;
-    if ((!postId && !commentId) || (postId && commentId)) {
-      res.status(400).send();
-    }
-
-    if (postId) {
-      const { likes, dislikes } = await countLikesByPostId(postId);
-      res.status(200).json({ likes, dislikes });
-    } else {
-      const { likes, dislikes } = await countLikesByCommentId(commentId);
-      res.status(200).json({ likes, dislikes });
-    }
-  } catch (err) {
-    res.status(500).send();
-  }
-};
-
-const likedByUser = async (req, res) => {
-  try {
-    const { postId, userId, commentId } = req.query;
-    if ((!postId && !commentId) || (postId && commentId)) {
-      res.status(400).send();
-    }
-
-    if (postId) {
-      const like = await PostLike.findOne({
-        where: {
-          postId,
-          userId,
+    // if not enough information provided
+    if (!type || !id || like === undefined) {
+      return res.status(400).json({
+        message: "Bad request",
+        fields: {
+          type: "post or comment",
+          id: "commentId or postId",
+          like: "true or false, indicate likes or dislikes",
         },
       });
-      if (like) {
-        res.status(200).json(like);
-      } else {
-        res.status(404).send();
+    }
+
+    // if type is not comment or post
+    if (type !== "comment" && type !== "post") {
+      return res
+        .status(400)
+        .json({ message: "Bad request", fields: { type: "post or comment" } });
+    }
+
+    // if like is not true or false
+    if (like !== "true" && like !== "false") {
+      return res
+        .status(400)
+        .json({ message: "Bad request", fields: { like: "true or false" } });
+    }
+
+    // if type is comment
+    if (type === "comment") {
+      // if comment does not exist
+      const comment = await Comment.findByPk(id);
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
       }
-    } else {
+      // if comment is on a post that is not published
+      const post = await Post.findByPk(comment.PostId);
+      if (post.status !== "PUBLISHED" && userId !== post.UserId) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+
+      // if like is true
+      if (like === "true") {
+        // get likes
+        const likes = await CommentLike.count({
+          where: { CommentId: id, isDislike: false },
+        });
+        return res.status(200).json({ likes });
+      } else {
+        // if like is false
+        const dislikes = await CommentLike.count({
+          where: { CommentId: id, isDislike: true },
+        });
+        return res.status(200).json({ dislikes });
+      }
+    }
+
+    // if type is post
+    if (type === "post") {
+      // if post does not exist
+      const post = await Post.findByPk(id);
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+      // if post is not published
+      if (post.status !== "PUBLISHED" && userId !== post.UserId) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+
+      // if like is true
+      if (like === "true") {
+        // get likes
+        const likes = await PostLike.count({
+          where: { PostId: id, isDislike: false },
+        });
+        return res.status(200).json({ likes });
+      } else {
+        // if like is false
+        const dislikes = await PostLike.count({
+          where: { PostId: id, isDislike: true },
+        });
+        return res.status(200).json({ dislikes });
+      }
+    }
+
+    return res.status(404).json({ message: "Not found" });
+  } catch (err) {
+    return res.status(500).json({ message: "Unexpected error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  if (!req.userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const { userId } = req;
+    const { type, id, like } = req.body;
+
+    // if not enough information provided
+    if (!type || !id || like === undefined) {
+      return res.status(400).json({
+        message: "Bad request",
+        fields: {
+          type: "post or comment",
+          id: "commentId or postId",
+          like: "true or false, indicate likes or dislikes",
+        },
+      });
+    }
+
+    // if type is not comment or post
+    if (type !== "comment" && type !== "post") {
+      return res
+        .status(400)
+        .json({ message: "Bad request", fields: { type: "post or comment" } });
+    }
+
+    // if like is not true or false
+    if (like !== true && like !== false) {
+      return res
+        .status(400)
+        .json({ message: "Bad request", fields: { like: "true or false" } });
+    }
+
+    // if type is comment
+    if (type === "comment") {
+      // if comment does not exist
+      const comment = await Comment.findByPk(id);
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+
+      // if like is true
+      if (like) {
+        // invalidate likes and dislikes of the user
+        await CommentLike.destroy({ where: { UserId: userId, CommentId: id } });
+        // like the comment
+        await CommentLike.create({ UserId: userId, CommentId: id });
+        return res.status(200).json({ message: "Comment liked" });
+      } else {
+        // dislike the comment
+        await CommentLike.destroy({ where: { UserId: userId, CommentId: id } });
+        await CommentLike.create({
+          UserId: userId,
+          CommentId: id,
+          isDislike: true,
+        });
+        return res.status(200).json({ message: "Comment disliked" });
+      }
+    }
+
+    // if type is post
+    if (type === "post") {
+      // if post does not exist
+      const post = await Post.findByPk(id);
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+
+      // if like is true
+      if (like) {
+        // invalidate likes and dislikes of the user
+        await PostLike.destroy({ where: { UserId: userId, PostId: id } });
+        // like the post
+        await PostLike.create({ UserId: userId, PostId: id });
+        return res.status(200).json({ message: "Post liked" });
+      } else {
+        await PostLike.destroy({ where: { UserId: userId, PostId: id } });
+        // dislike the post
+        await PostLike.create({ UserId: userId, PostId: id, isDislike: true });
+        return res.status(200).json({ message: "Post disliked" });
+      }
+    }
+
+    return res.status(404).json({ message: "Not found" });
+  } catch (err) {
+    return res.status(500).json({ message: "Unexpected error" });
+  }
+});
+
+router.get("/did", async (req, res) => {
+  // type, id from query
+  // userId from req
+  const { userId } = req;
+  if (!userId) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const { type, id } = req.query;
+
+    // if not enough information provided
+    if (!type || !id) {
+      return res.status(400).json({
+        message: "Bad request",
+        fields: {
+          type: "post or comment",
+          id: "commentId or postId",
+        },
+      });
+    }
+
+    // if type is not comment or post
+    if (type !== "comment" && type !== "post") {
+      return res
+        .status(400)
+        .json({ message: "Bad request", fields: { type: "post or comment" } });
+    }
+
+    // if type is comment
+    if (type === "comment") {
+      // if comment does not exist
+      const comment = await Comment.findByPk(id);
+      if (!comment) {
+        return res.status(404).json({ message: "Comment not found" });
+      }
+
+      // if user liked the comment
       const like = await CommentLike.findOne({
-        where: {
-          commentId,
-          userId,
-        },
+        where: { UserId: userId, CommentId: id },
       });
       if (like) {
-        res.status(200).json(like);
+        return res.status(200).json({ did: true });
       } else {
-        res.status(404).send();
+        return res.status(200).json({ did: false });
       }
     }
+
+    // if type is post
+    if (type === "post") {
+      // if post does not exist
+      const post = await Post.findByPk(id);
+      if (!post) {
+        return res.status(404).json({ message: "Post not found" });
+      }
+
+      // if user liked the post
+      const like = await PostLike.findOne({
+        where: { UserId: userId, PostId: id },
+      });
+      if (like) {
+        return res.status(200).json({ did: true });
+      } else {
+        return res.status(200).json({ did: false });
+      }
+    }
+
+    return res.status(404).json({ message: "Not found" });
   } catch (err) {
-    res.status(500).send();
+    return res.status(500).json({ message: "Unexpected error" });
   }
-};
-
-const like = async (req, res) => {
-  try {
-    const { postId, userId, isDislike, commentId } = req.body;
-    if ((!postId && !commentId) || (postId && commentId)) {
-      res.status(400).send();
-    }
-
-    if (postId) {
-      await likePost(postId, userId, isDislike);
-    } else {
-      await likeComment(commentId, userId, isDislike);
-    }
-
-    res.status(200).send();
-  } catch (err) {
-    res.status(500).send();
-  }
-};
-
-const unlike = async (req, res) => {
-  try {
-    const { postId, userId, commentId } = req.query;
-    if ((!postId && !commentId) || (postId && commentId)) {
-      res.status(400).send();
-    }
-
-    if (postId) {
-      await unlikePost(postId, userId);
-    } else {
-      await unlikeComment(commentId, userId);
-    }
-
-    res.status(200).send();
-  } catch (err) {
-    res.status(500).send();
-  }
-};
-
-router.get("/", getLikes);
-router.get("/liked", likedByUser);
-router.put("/", JwtMiddleware, like);
-router.delete("/", JwtMiddleware, unlike);
+});
 
 module.exports = router;
