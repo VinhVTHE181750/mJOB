85413a27475ba76167b392940b18a7ff9b7535d4
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-07-12 22:42:55 +0700

    Temporary disable xs view of forum

--- client/src/components/forum/PostSearch.jsx
+++ client/src/components/forum/PostSearch.jsx
@@ -1,14 +1,31 @@
 import PropTypes from "prop-types";
-import {Button, Col, Dropdown, Form, Row} from "react-bootstrap";
-import {FaSearch} from "react-icons/fa";
-import {FaFilter, FaSort} from "react-icons/fa6";
-import {useContext} from "react";
+import { Button, Col, Dropdown, Form, Row } from "react-bootstrap";
+import { FaSearch } from "react-icons/fa";
+import { FaFilter, FaSort } from "react-icons/fa6";
+import { useContext, useEffect } from "react";
 import Tag from "./micro/Tag";
-import {ForumContext} from "../../context/ForumContext";
+import { ForumContext } from "../../context/ForumContext";
 
 const PostSearch = () => {
-  const { searchTerms, removeTag, addTag, setUser } = useContext(ForumContext);
+  const {
+    searchTerms,
+    removeTag,
+    addTag,
+    setUser,
+    unsetUser,
+    setTitle,
+    unsetTitle,
+    setContent,
+    unsetContent,
+    setCategory,
+    unsetCategory,
+    clearSearchTerms,
+  } = useContext(ForumContext);
   const searchTags = searchTerms.tags;
+  const category = searchTerms.category;
+  const user = searchTerms.username;
+  const title = searchTerms.title;
+  const content = searchTerms.content;
 
   const handleChange = (e) => {
     // if last characte r entered was space, parse the entered word as a tag if it start with #
@@ -16,75 +33,69 @@ const PostSearch = () => {
     // if last char   is not alphanumeric
     const regex = /^[a-zA-Z0-9]+$/;
     if (regex.test(lastChar) === false) {
-      // cut the last char
       const query = e.target.value.slice(0, -1);
-      if (query[0] === "#") {
-        addTag(query.substring(1));
-        e.target.value = "";
+      const type = query[0];
+      const value = query.slice(1);
+      if (regex.test(value) === false) {
+        return;
       }
-      if (query[0] === "@") {
-        setUser(query.substring(1));
-        e.target.value = "";
+      switch (type) {
+        case "#":
+          addTag(value);
+          e.target.value = "";
+          break;
+        case "@":
+          setUser(value);
+          e.target.value = "";
+          break;
+        case "$":
+          setContent(value);
+          e.target.value = "";
+          break;
+        default:
+          break;
       }
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    clearSearchTerms();
+  };
+
+  useEffect(() => {
+    
+  }, [searchTerms]);
+
   return (
-    <Form>
+    <>
       <Row className="align-items-center">
-        {/* <Col xs="auto"> */}
-        {/* {category ? <Category category={category} /> : <Category />} */}
-        {/* </Col> */}
-        <Col xs={'auto'} className="me-0 pe-0">
-        
-          <Form.Control
-            type="text"
-            placeholder="Enter a query..."
-            onChange={handleChange}
-          />
-        </Col>
-        <Col xs={'auto'} className="px-0 mx-0">
-        
-          <Dropdown>
-            <Dropdown.Toggle variant="outline-secondary">
-              <FaSort /> Sort
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item>Featured</Dropdown.Item>
-              <Dropdown.Item>Title (ascending)</Dropdown.Item>
-              <Dropdown.Item>Title (descending)</Dropdown.Item>
-              <Dropdown.Item>Date (ascending)</Dropdown.Item>
-              <Dropdown.Item>Date (descending)</Dropdown.Item>
-              <Dropdown.Item>Most likes</Dropdown.Item>
-              <Dropdown.Item>Most comments</Dropdown.Item>
-              <Dropdown.Item>Most views</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Col>
-        <Col xs={1} className="px-0 mx-0">
-          <Dropdown>
-            <Dropdown.Toggle variant="outline-secondary">
-              <FaFilter /> Filter
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item>Today</Dropdown.Item>
-              <Dropdown.Item>This week</Dropdown.Item>
-              {/* Add more filter options here */}
-            </Dropdown.Menu>
-          </Dropdown>
-        </Col>
-        <Col xs={2} className="px-0 mx-0">
-          <Button variant="primary">
-            <FaSearch /> Search
-          </Button>
+        {/* <Col xs="auto">
+        {category ? <Category category={category} /> : <Category />}
+        </Col> */}
+        <Col xs={10} sm={8} md={6} className="">
+          <Form onSubmit={handleSubmit}>
+            <Form.Control className="d-sm-inline"
+              type="text"
+              placeholder="Enter title query or a #tag, @user, $content..."
+              onChange={handleChange}
+            />
+            <Button type="submit" className="d-none d-sm-inline">Submit</Button>
+          </Form>
         </Col>
       </Row>
       <Row className="mx-auto mt-1 me-auto">
+        {user && <Tag key="user" tag={`@${user} ❌`} handler={unsetUser} />}
         {searchTags.map((tag) => (
           <Tag key={tag} tag={`${tag} ❌`} handler={() => removeTag(tag)} />
         ))}
       </Row>
-    </Form>
+    </>
   );
 };
 
--- client/src/pages/forum/Forum.jsx
+++ client/src/pages/forum/Forum.jsx
@@ -1,43 +1,32 @@
-import {Col, Container, Row} from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import PostSearch from "../../components/forum/PostSearch";
 import NavigateButton from "../../components/ui/buttons/NavigateButton";
 import ListPost from "../../components/forum/ListPost";
 import ChatBox from "../../components/forum/ChatBox";
-import {useState} from "react";
+import { useState } from "react";
 
 const Forum = () => {
   const [tags, setTags] = useState([]);
   const addSearchTag = (tag) => {
     if (!tags.includes(tag)) {
       setTags([...tags, tag]);
-      console.log('added ' + tag)
+      console.log("added " + tag);
     }
   };
   const removeSearchTag = (tag) => {
     setTags(tags.filter((t) => t !== tag));
-    console.log('removed ' + tag)
+    console.log("removed " + tag);
   };
   return (
-    <Container>
-      <Row>
-        <Col md={12}>
-          <Row className="align-items-center">
-            <Col xs={12} md={10} className="">
-              <PostSearch tagHandler={removeSearchTag} tags={tags} />
-            </Col>
-            <Col md={2} className="d-flex flex-row-reverse">
-              <NavigateButton
-                path="/forum/add"
-                variant="success"
-                text="Add Post"
-              >
-                Navigate
-              </NavigateButton>
-            </Col>
-          </Row>
-          <ListPost tagHandler={addSearchTag} />
-        </Col>
-      </Row>
+    <Container className="d-none d-sm-block">
+      <PostSearch tagHandler={removeSearchTag} tags={tags} />
+      <div className="d-flex flex-row-reverse">
+        <NavigateButton path="/forum/add" variant="success" text="Add Post">
+          Navigate
+        </NavigateButton>
+      </div>
+
+      <ListPost tagHandler={addSearchTag} />
       <ChatBox user={"user1"} />
     </Container>
   );
