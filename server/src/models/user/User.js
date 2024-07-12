987ac867412ed22fe7fdf05e55ca0053d5578b4e
Pham Duc Minh <minhpdhe181302@fpt.edu.vn>
2024-07-12 09:03:44 +0700

    update user profile

--- server/src/models/user/User.js
+++ server/src/models/user/User.js
@@ -9,6 +9,7 @@ const { Model, DataTypes } = require("sequelize");
 const PublicRoomUser = require("../forum/chat/PublicRoomUser");
 const PublicChatRoom = require("../forum/chat/PublicChatRoom");
 const Balance = require("../payment/Balance");
+const Profile = require("./Profile");
 
 class User extends Model {}
 
@@ -94,6 +95,9 @@ PublicChatRoom.belongsToMany(User, { through: PublicRoomUser });
 
 User.hasOne(Balance);
 Balance.belongsTo(User);
+
+User.hasOne(Profile);
+Profile.belongsTo(User);
 
 // missing LinkedProfile
 
