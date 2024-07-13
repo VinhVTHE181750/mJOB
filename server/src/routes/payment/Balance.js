b264c1cf96421ecd3812c097955de220c75d87dd
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-07-13 23:27:58 +0700

    Update payment APIs

--- server/src/routes/payment/Balance.js
+++ server/src/routes/payment/Balance.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Balance = require("../../models/payment/Balance");
+const { log } = require("../../utils/Logger");
+// Import logPayment from PaymentLogger.js
+const { logPayment } = require("../../utils/PaymentLogger"); // Adjust the path as necessary
 
 const getBalance = async (req, res) => {
   if (!req.userId) return res.status(401).json({ error: "Unauthorized" });
@@ -10,9 +13,12 @@ const getBalance = async (req, res) => {
     });
     if (!balance) {
       await Balance.create({ UserId: req.userId, balance: 0 });
+      // Log the OPEN action using logPayment
+      await logPayment(req.userId, "OPEN", 0, null, null, "SUCCESS");
       return res.json({ balance: 0, message: "Balance created" });
     } else return res.json({ balance: balance.balance });
   } catch (error) {
+    log(error, "ERROR", "sequelize");
     return res
       .status(500)
       .json({ message: "Unexpected error while fetching balance" });
