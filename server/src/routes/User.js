755e50907f7e948e02ee73f9aed115a2918c9ecd
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-07-15 16:40:43 +0700

    Add api/whoami to get userId, role, username

--- server/src/routes/User.js
+++ server/src/routes/User.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { JwtMiddleware } = require("../utils/JWT");
 
-const { getAllUsers } = require("./user/User");
+const { getAllUsers, getUser, updateUser } = require("./user/User");
 
 router.get("/", getAllUsers);
+router.get("/:id", getUser);
+router.put("/:id", updateUser)
+
 
 module.exports = router;
