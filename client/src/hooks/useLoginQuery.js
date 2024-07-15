5e53066f5253677fcd7ea4fc845024ec32324891
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-07-15 14:48:16 +0700

    Remove console.log from client auth

--- client/src/hooks/useLoginQuery.js
+++ client/src/hooks/useLoginQuery.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import http from '../functions/httpService'; // Import the instance
+import http from "../functions/httpService"; // Import the instance
 import { useAuth } from "../context/UserContext";
 
 export const useLoginQuery = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const {setIsLogin} = useAuth()
+  const { setIsLogin } = useAuth();
 
   const login = async (username, password) => {
     setLoading(true);
@@ -14,7 +14,7 @@ export const useLoginQuery = () => {
     try {
       const response = await http.post("/auth/login", { username, password });
       if (response.status === 200) {
-        setIsLogin(true)
+        setIsLogin(true);
       }
       return response.status === 200;
     } catch (err) {
