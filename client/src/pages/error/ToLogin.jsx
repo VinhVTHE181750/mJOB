b45cddc2dceff3ec374ef4208d57797a948ade51
HE181750 Vương Tuấn Vinh <vtvinh24@gmail.com>
2024-07-16 17:28:36 +0700

    Add a middle-page between login and other

--- client/src/pages/error/ToLogin.jsx
+++ client/src/pages/error/ToLogin.jsx
@@ -1,14 +1,60 @@
+import PropTypes from "prop-types";
 import NavigateButton from "../../components/ui/buttons/NavigateButton";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+import useWhoAmI from "../../hooks/user/useWhoAmI";
+import { useNavigate } from "react-router";
 
-const ToLogin = () => {
-  return (
+const ToLogin = ({ path }) => {
+  const { loggedIn } = useContext(AuthContext);
+  const { role, loading } = useWhoAmI();
+  const navigate = useNavigate();
+
+  if (loading)
+    return (
+      <Container className="d-flex justify-content-center justify-items-center align-items-center">
+        <Spinner
+          animation="border"
+          role="status"
+        />
+      </Container>
+    );
+
+  if (loggedIn) {
+    if (path) {
+      navigate(path);
+    } else {
+      if (role) {
+        switch (role) {
+          case "ADMIN": {
+            navigate("/dashboard");
+            break;
+          }
+          default: {
+            navigate("/home");
+          }
+        }
+      }
+    }
+  }
+
+  return role ? (
+    {}
+  ) : (
     <Container>
       <h1 className="text-center">Unauthorized</h1>
       <h5>You must login to do this.</h5>
-      <NavigateButton path="/login" text="Login" />
+      <NavigateButton
+        path="/login"
+        text="Login"
+      />
     </Container>
   );
+};
+
+ToLogin.propTypes = {
+  path: PropTypes.string,
 };
 
 export default ToLogin;
