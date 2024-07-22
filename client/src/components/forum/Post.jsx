18ea3186bb9cac4e23c8067e0260e55e7b2c6d7d
HE181750 Vương Tuấn Vinh <vinhvthe181750@fpt.edu.vn>
2024-07-22 21:21:33 +0700

    Update forum components

--- client/src/components/forum/Post.jsx
+++ client/src/components/forum/Post.jsx
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
+import { Button, Col, Container, FloatingLabel, Form, Row } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import avatar from "../../assets/img/default_avatar.webp";
 import { getMoment } from "../../functions/Converter";
 import NavigateButton from "../ui/buttons/NavigateButton";
 // import { useDispatch, useSelector } from "react-redux";
 // import { deletePost, fetchPost } from "../../store/reducers/forum/postReducer";
-import { BsExclamation } from "react-icons/bs";
+import { BsExclamation, BsPencilFill, BsTrash3 } from "react-icons/bs";
 import Skeleton from "react-loading-skeleton";
 import useLikesUpdate from "../../hooks/forum/likes/useLikesUpdate";
 import usePostDelete from "../../hooks/forum/posts/usePostDelete";
@@ -15,6 +15,11 @@ import usePostDetail from "../../hooks/forum/posts/usePostDetail";
 import useWhoAmI from "../../hooks/user/useWhoAmI";
 import socket from "../../socket";
 import LikeButton from "./micro/LikeButton";
+import Tag from "./micro/Tag";
+import Category from "./micro/Category";
+import { ForumContext } from "../../context/ForumContext";
+import useCommentInsert from "../../hooks/forum/comments/useCommentInsert";
+import ListComment from "./ListComment";
 
 const Post = ({ id }) => {
   // post related data
@@ -27,6 +32,7 @@ const Post = ({ id }) => {
   const { updateLikes } = useLikesUpdate();
   const { username } = useWhoAmI();
   const { deletePost } = usePostDelete();
+  const { categoryOf } = useContext(ForumContext);
 
   // useEffect(() => {
   //   dispatch(fetchPost(id));
@@ -53,10 +59,6 @@ const Post = ({ id }) => {
     };
   }, [id]);
 
-  const handleLike = (isLike) => {
-    updateLikes("post", id, isLike);
-  };
-
   const handleDeletePost = async () => {
     const confirmDelete = window.confirm("Are you sure you want to delete this post? ");
     if (confirmDelete) {
@@ -66,84 +68,104 @@ const Post = ({ id }) => {
     }
   };
 
+  const [comment, setComment] = useState("");
+  const { insertComment } = useCommentInsert();
+
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    insertComment(comment, Number(id));
+    setComment("");
+  };
+
+  const handleCommentChange = (e) => {
+    setComment(e.target.value);
+  };
+
   if (error) {
-    return (
-      <>
-        <h3 className="text-center">Something went wrong while fetching the post.</h3>
-        <div className="d-flex align-items-center justify-content-center">
-          <NavigateButton
-            path="/report"
-            text="Report this problem"
-            variant="danger"
-            icon={<BsExclamation />}
-          />
+    if (error.response.status === 404) {
+      return (
+        <div className="text-center">
+          <h3>Post not found</h3>
         </div>
-      </>
-    );
+      );
+    } else {
+      return (
+        <>
+          <h3 className="text-center">Something went wrong while fetching the post.</h3>
+          <div className="d-flex align-items-center justify-content-center">
+            <NavigateButton
+              path="/report"
+              text="Report this problem"
+              variant="danger"
+              icon={<BsExclamation />}
+            />
+          </div>
+        </>
+      );
+    }
   }
 
   if (loading) {
     return (
-      <>
-        <div className="post">
-          <Row className="post-title">
-            <Col sm={2}>
-              <div className="border">
-                <div>
-                  <Skeleton
-                    circle={true}
-                    height={50}
-                    width={50}
-                  />
-                </div>
-                <Skeleton
-                  width={100}
-                  height={20}
-                />
-                <div>
-                  <p className="text-center">
-                    <Skeleton
-                      width={100}
-                      height={20}
-                    />
-                  </p>
-                </div>
-              </div>
-            </Col>
-            <Col sm={10}>
-              <Row className="mx-5">
-                <Skeleton
-                  width={300}
-                  height={30}
-                />
-              </Row>
-              <Row className="mx-5 mt-2">
-                <Skeleton
-                  width={250}
-                  height={20}
-                />
-              </Row>
-            </Col>
-          </Row>
-          <Row className="ms-2 mt-2">
-            <Row>
-              <Skeleton height={200} />
-            </Row>
-            <Row>
-              <div className="d-flex justify-content-end mt-2 gap-2">
-                <Skeleton
-                  width={100}
-                  height={40}
-                />
-                <Skeleton
-                  width={100}
-                  height={40}
-                />
-              </div>
-            </Row>
-          </Row>
-        </div>
-      </>
+      <Container className="rounded">
+        <Row className="flex flex-row-reverse bg-white border border-info rounded p-2">
+          <Col>
+            <Skeleton
+              height={30}
+              width={300}
+            />
+          </Col>
+          <Col sm="auto">
+            <Skeleton
+              circle={true}
+              height={50}
+              width={50}
+            />
+          </Col>
+        </Row>
+        <Row className="mt-2 border rounded border-info bg-white">
+          <Col>
+            <Skeleton count={5} />
+          </Col>
+        </Row>
+        <Row className="mt-2">
+          <Col
+            xs={12}
+            sm={3}
+            className="bg-white rounded border border-success"
+          >
+            <div className="d-flex justify-content-center">
+              <Skeleton
+                circle={true}
+                height={100}
+                width={100}
+              />
+            </div>
+            <Skeleton
+              height={30}
+              width={100}
+            />
+          </Col>
+          <Col>
+            <Skeleton
+              width={80}
+              height={20}
+            />
+          </Col>
+          <Col sm="auto pe-0">
+            <Skeleton
+              width={80}
+              height={40}
+              className="me-2"
+            />
+            <Skeleton
+              width={80}
+              height={40}
+              className="me-2"
+            />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 
@@ -152,68 +174,104 @@ const Post = ({ id }) => {
   }
 
   return (
-    <div className="post">
-      <Row className="post-title">
-        <Col sm={2}>
-          <div className="border border-success">
-            <div>
+    <Container className="rounded">
+      <Row className="flex flex-row-reverse bg-white border border-info rounded p-2">
+        <Col>
+          <h2 className="text-center">{post.title}</h2>
+        </Col>
+        <Col sm="auto">
+          <Category category={categoryOf(post.id)} />
+        </Col>
+      </Row>
+      <Row className="mt-2 border rounded border-info bg-white">
+        <Col>
+          <pre className="text-wrap">{post.content}</pre>
+        </Col>
+      </Row>
+      <Row className="mt-2 mb-5">
+        <Col
+          xs={12}
+          sm={3}
+          className="bg-white rounded border border-success"
+        >
+          <div
+            role="button"
+            onClick={() => navigate(`/profile/${post.author}`)}
+          >
+            <div className="d-flex justify-content-center">
               <img
-                className="avatar"
-                src={avatar}
-                alt="Default Avatar"
+                src={post.avatar || avatar}
+                alt="avatar"
+                width={100}
+                height={100}
               />
             </div>
-            <Link to={`/users/${post.author}`}>{post.author}</Link>
-            <div>
-              <p className="text-center">{getMoment(post.updatedAt)}</p>
-            </div>
+            <h5 className="text-center text-primary pointer">{post.author}</h5>
           </div>
+          <p className="text-center">Last update: {getMoment(post.updatedAt)}</p>
         </Col>
-        <Col sm={10}>
-          <Row className="mx-5">
-            <h1>{post.title} </h1>
-          </Row>
+        <Col>
+          <Tag tag="tag1" />
+        </Col>
+        <Col sm="auto pe-0">
+          <LikeButton
+            id={Number(id)}
+            type="post"
+            action="like"
+            count="{post.likes}"
+            className="me-2"
+          />
+          <LikeButton
+            id={Number(id)}
+            type="post"
+            action="dislike"
+            count="{post.dislikes}"
+            className="me-2"
+          />
           {username === post.author && (
             <>
               <NavigateButton
                 path={`/forum/edit/${id}`}
-                text="Edit"
                 variant="primary"
                 className="me-2"
-              />
+              >
+                <BsPencilFill /> Edit
+              </NavigateButton>
               <Button
                 variant="danger"
                 onClick={() => handleDeletePost()}
               >
-                Delete
+                <BsTrash3 /> Delete
               </Button>
             </>
           )}
         </Col>
       </Row>
-      <Row className="ms-2 mt-2">
-        <Row>
-          <div className="border mt-4">{post.content}</div>
-        </Row>
-        <Row>
-          {/* Add a like button */}
-          <div className="d-flex justify-content-end mt-2 gap-2">
-            <LikeButton
-              id={Number(id)}
-              type="post"
-              action="like"
-              count={post.likes}
-            />
-            <LikeButton
-              id={Number(id)}
-              type="post"
-              action="dislike"
-              count={post.dislikes}
-            />
-          </div>
-        </Row>
-      </Row>
-    </div>
+
+      <ListComment id={Number(post.id)} />
+
+      {/* Comment Form */}
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Add a comment"
+      >
+        <Form.Control
+          as="textarea"
+          value={comment}
+          onChange={handleCommentChange}
+          placeholder="Leave a comment here"
+          className="border border-info"
+          size="lg"
+        />
+        <Button
+          variant="primary"
+          className="mt-2 mb-2"
+          onClick={handleCommentSubmit}
+        >
+          Submit
+        </Button>
+      </FloatingLabel>
+    </Container>
   );
 };
 
