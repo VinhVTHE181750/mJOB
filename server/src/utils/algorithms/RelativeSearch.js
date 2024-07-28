9d5ace1873125e2eb3bb8ff2c85e4586312f0871
HE181750 Vương Tuấn Vinh <vinhvthe181750@fpt.edu.vn>
2024-07-28 22:15:52 +0700

    Update RelativeSearch.js
    Added relativePostSearch()

--- server/src/utils/algorithms/RelativeSearch.js
+++ server/src/utils/algorithms/RelativeSearch.js
@@ -1,5 +1,6 @@
 const Job = require("../../models/job/Job");
 const JobPreference = require("../../models/user/JobPreference");
+const PostPreference = require("../../models/user/PostPreference");
 const User = require("../../models/user/User");
 
 /**
@@ -112,4 +113,94 @@ async function relativeJobSearch(id, jobList) {
   return result;
 }
 
-module.exports = relativeJobSearch;
+/**
+ *
+ * @param {number} id the id of the user who is searching for a post
+ * @param {Array} postList the list of posts that the user can search for
+ *
+ * @returns {Array} the list of posts that are most relevant to the user
+ *
+ * If the input post list contains fewer than 5 posts, return the list as is
+ * If the user does not have a post preference, return the list as is
+ *
+ * The relevance is calculated by scores:
+ * - Tags: for each matched keywords, 1 point
+ * - Categories: for each matched keywords, 5 point
+ */
+async function relativePostSearch(id, postList) {
+  // if postList is not an array, return null
+  if (!Array.isArray(postList)) {
+    return null;
+  }
+
+  // if user with id is not found, return null
+  const user = await User.findByPk(id);
+  if (!user) {
+    return null;
+  }
+
+  const userPreference = await PostPreference.findByPk(id);
+
+  // if userPreference is not found, return the list as is
+  if (!userPreference) {
+    return postList;
+  }
+
+  // If postList has fewer than 5 posts, do not sort
+  if (postList.length < 5) {
+    return postList;
+  }
+
+  const { tags, categories } = userPreference;
+
+  let scoreList = [];
+  for (let post of postList) {
+    let score = 0;
+
+    // calculate scores
+    // 1. Tags
+    // Example: tags = "it,software,developer"
+    // post.tags = "it,software,developer"
+    // post.tags = "it,software"
+    if (tags) {
+      const tagList = tags.split(",");
+      const postTagList = post.tags.split(",");
+      for (let tag of tagList) {
+        if (postTagList.includes(tag)) {
+          score += 1;
+        }
+      }
+    }
+
+    // 2. Categories
+    // Example: categories = "General,Job,Guide"
+    // post.categories = "General,Job,Guide"
+    // post.categories = "General,Job"
+    if (categories) {
+      const categoryList = categories.split(",");
+      const postCategoryList = post.categories.split(",");
+      for (let category of categoryList) {
+        if (postCategoryList.includes(category)) {
+          score += 5;
+          break;
+        }
+      }
+    }
+
+    // update the record with the score
+    scoreList.push({ post, score });
+  }
+
+  // sort the postList by score
+  scoreList.sort((a, b) => b.score - a.score);
+  let result = [];
+  for (let i = 0; i < scoreList.length; i++) {
+    result.push(scoreList[i].post);
+  }
+  return result;
+}
+
+module.exports = {
+  relativeJobSearch,
+  relativePostSearch,
+};
