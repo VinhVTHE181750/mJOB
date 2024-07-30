e16a42f0c00b2944babb0c3aaa8891a9ca66f6af
HE181750 Vương Tuấn Vinh <vinhvthe181750@fpt.edu.vn>
2024-07-30 10:21:02 +0700

    Integrate with OpenAI API
    Added two functions in utils/OpenAI.js
    1. calcRelevance: calculate relevanceScore and harmfulnessScore of input type, title, content
    2. generateResponse: query a chatgpt-3.5-turbo response for input prompt

--- server/src/routes/test.js
+++ server/src/routes/test.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { relevantJobSearch } = require("../utils/algorithms/RelativeSearch");
 const Job = require("../models/job/Job");
 const { log } = require("../utils/Logger");
+const { generateResponse, calcRelevance } = require("../utils/OpenAI");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -16,6 +17,28 @@ router.get("/relevant-jobs/:id", async (req, res) => {
     if (!result) {
       return res.status(400).send("No relevant jobs found");
     }
+    return res.status(200).send(result);
+  } catch (error) {
+    log(error, "ERROR", "TEST");
+    return res.status(500).send(error.message);
+  }
+});
+
+router.post("/chat", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await generateResponse(prompt);
+    return res.status(200).send(response);
+  } catch (error) {
+    log(error, "ERROR", "TEST");
+    return res.status(500).send(error.message);
+  }
+});
+
+router.post("/calc", async (req, res) => {
+  try {
+    const { type, title, content } = req.body;
+    const result = await calcRelevance(type, title, content);
     return res.status(200).send(result);
   } catch (error) {
     log(error, "ERROR", "TEST");
