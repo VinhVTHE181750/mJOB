fa7a4d67b4f756681a5b20390ecef4ddac2b7ad6
HE181750 Vương Tuấn Vinh <vinhvthe181750@fpt.edu.vn>
2024-07-30 21:23:21 +0700

    Update EmployerProfile.js

--- server/src/models/user/EmployerProfile.js
+++ server/src/models/user/EmployerProfile.js
@@ -5,10 +5,43 @@ class EmployerProfile extends Model {}
 
 EmployerProfile.init(
   {
+    // basic employer info
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    // org info
+    organization: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    industry: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    taxId: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    founded: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    size: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+
+    // contact
     email: {
       type: DataTypes.STRING,
       allowNull: true,
@@ -21,17 +54,27 @@ EmployerProfile.init(
       type: DataTypes.STRING,
       allowNull: true,
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    avatar: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
     website: {
       type: DataTypes.STRING,
       allowNull: true,
+    },
+
+    // platform metadata
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isVerified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isMuted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isLocked: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   },
   {
